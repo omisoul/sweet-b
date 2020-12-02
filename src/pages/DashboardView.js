3709bd1283cc10daffc7ff3daa99881cd7ebed8d
omisoul <marlonjames24@gmail.com>
2020-12-02 14:11:42 -0500

    Fixed spelling error

--- src/pages/DashboardView.js
+++ src/pages/DashboardView.js
@@ -3,7 +3,7 @@ import UsersListItem from '../components/UsersListItem';
 import { UsersListContext } from '../providers/UsersListProvider';
 
 const DashboardView = () => {
-  const users = useContext(USersListContext);
+  const users = useContext(UsersListContext);
   return (
     <div className="user-list-page">
       <p>Test</p>
