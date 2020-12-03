f08a6a0f95879a5342db81f203e6bd8e4db1d5bf
Rojae Martin <rojaedmartin@gmail.com>
2020-12-03 07:35:36 -0500

    Modified Dashboard

--- src/pages/DashboardView.js
+++ src/pages/DashboardView.js
@@ -1,12 +1,93 @@
-import React, { useContext } from 'react';
-import UsersListItem from '../components/UsersListItem';
+import React, { useContext, useEffect, useState } from 'react';
 import { UsersListContext } from '../providers/UsersListProvider';
+import { ProductsContext } from '../providers/ProductsProvider';
+import { OrdersContext } from '../providers/OrdersProvider';
 
 const DashboardView = () => {
   const users = useContext(UsersListContext);
+  const products = useContext(ProductsContext);
+  const orders = useContext(OrdersContext);
+
+  const [numUsers, setNumUsers] = useState(0);
+  const [numOrders, setNumOrders] = useState(0);
+  const [numPending, setNumPending] = useState(0);
+  const [numDelivered, setNumDelivered] = useState(0);
+  const [numCanceled, setNumCanceled] = useState(0);
+  const [numProducts, setNumProducts] = useState(0);
+
+  useEffect(() => {
+    let total = 0;
+    for (let i of users) {
+      total += 1;
+    }
+    setNumUsers(total);
+  }, [users]);
+
+  useEffect(() => {
+    let total = 0;
+    for (let i of products) {
+      total += 1;
+    }
+    setNumProducts(total);
+  }, [products]);
+
+  useEffect(() => {
+    let total = 0;
+    let totPending = 0;
+    let totDelivered = 0;
+    let totCanceled = 0;
+
+    for (let i of orders) {
+      total += 1;
+      if (i.status == 'Pending') {
+        totPending += 1;
+      } else if (i.status == 'Delivered') {
+        totDelivered += 1;
+      } else {
+        totCanceled += 1;
+      }
+    }
+    setNumOrders(total);
+    setNumCanceled(totCanceled);
+    setNumDelivered(totDelivered);
+    setNumPending(totPending);
+  }, [orders]);
+
   return (
     <div className="user-list-page">
-      <p>Test</p>
+      <h1>Administrative Dashboard</h1>
+
+      <div className="dashboard-items">
+        <div className="dashboard-card">
+          <h4>Users</h4>
+          <p>{numUsers}</p>
+        </div>
+
+        <div className="dashboard-card">
+          <h4>Products</h4>
+          <p>{numProducts}</p>
+        </div>
+
+        <div className="dashboard-card">
+          <h4>Orders</h4>
+          <p>{numOrders}</p>
+        </div>
+
+        <div className="dashboard-card">
+          <h4>Pending Orders</h4>
+          <p>{numPending}</p>
+        </div>
+
+        <div className="dashboard-card">
+          <h4>Delivered Orders</h4>
+          <p>{numDelivered}</p>
+        </div>
+
+        <div className="dashboard-card">
+          <h4>Canceled Orders</h4>
+          <p>{numCanceled}</p>
+        </div>
+      </div>
     </div>
   );
 };
